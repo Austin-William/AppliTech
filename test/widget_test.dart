8d88bd03970f49f41337d0305af4cafeb4e607a9
Austin-William Lo <austin-william.lo@epitech.eu>
2021-12-22 20:25:13 +0100

    [ADD] some tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,63 +10,66 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets(
-    'Counter increments smoke test',
+    'AppliTech Widget smoke test',
     (WidgetTester tester) async {
       // Build our app and trigger a frame.
       await tester.pumpWidget(const AppliTech());
-
-      // Verify that our counter starts at 0.
-      expect(find.text('0'), findsOneWidget);
-      expect(find.text('1'), findsNothing);
-
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(const Home());
-
-      // Verify that our counter starts at 0.
-      expect(find.text('0'), findsOneWidget);
-      expect(find.text('1'), findsNothing);
-
+    },
+  );
+  testWidgets(
+    'Login Widget smoke test',
+    (WidgetTester tester) async {
       // Build our app and trigger a frame.
       await tester.pumpWidget(const Login());
-
-      // Verify that our counter starts at 0.
-      expect(find.text('0'), findsOneWidget);
-      expect(find.text('1'), findsNothing);
-
+    },
+  );
+  testWidgets(
+    'Home Widget smoke test',
+    (WidgetTester tester) async {
       // Build our app and trigger a frame.
-      await tester.pumpWidget(const SettingsPage());
-
-      // Verify that our counter starts at 0.
-      expect(find.text('0'), findsOneWidget);
-      expect(find.text('1'), findsNothing);
-
+      await tester.pumpWidget(const Home());
+    },
+  );
+  testWidgets(
+    'Notes page smoke test',
+    (WidgetTester tester) async {
       // Build our app and trigger a frame.
       await tester.pumpWidget(const NotesPage());
-
-      // Verify that our counter starts at 0.
-      expect(find.text('0'), findsOneWidget);
-      expect(find.text('1'), findsNothing);
-
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(const CalendarPage());
-
-      // Verify that our counter starts at 0.
-      expect(find.text('0'), findsOneWidget);
-      expect(find.text('1'), findsNothing);
-
+    },
+  );
+  testWidgets(
+    'Profile page smoke test',
+    (WidgetTester tester) async {
       // Build our app and trigger a frame.
       await tester.pumpWidget(const ProfilePage());
-
-      // Verify that our counter starts at 0.
-      expect(find.text('0'), findsOneWidget);
-      expect(find.text('1'), findsNothing);
-
+    },
+  );
+  testWidgets(
+    'Calendar page smoke test',
+    (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const CalendarPage());
+    },
+  );
+  testWidgets(
+    'Help page smoke test',
+    (WidgetTester tester) async {
       // Build our app and trigger a frame.
       await tester.pumpWidget(const HelpPage());
-
-      // Verify that our counter starts at 0.
-      expect(find.text('0'), findsOneWidget);
-      expect(find.text('1'), findsNothing);
+    },
+  );
+  testWidgets(
+    'AppliTech Widget smoke test',
+    (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const AppliTech());
+    },
+  );
+  testWidgets(
+    'Settings page smoke test',
+    (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const SettingsPage());
     },
   );
 }
