b28711fe4d8b71918679c25bf39013e98f85027e
Austin-William Lo <austin-william.lo@epitech.eu>
2022-02-18 20:44:35 +0100

    [ADD] unit test

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -16,99 +16,105 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 50),
-          child: Image.asset(
-            'assets/epitech_logo.png',
-            height: 100,
-            width: 100,
+    return Material(
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 50),
+            child: Image.asset(
+              'assets/epitech_logo.png',
+              height: 100,
+              width: 100,
+            ),
           ),
-        ),
-        Expanded(
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            constraints: const BoxConstraints.expand(),
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(35),
-                topRight: Radius.circular(35),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              constraints: const BoxConstraints.expand(),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(35),
+                  topRight: Radius.circular(35),
+                ),
+              ),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Flexible(
+                    child: TextField(
+                      obscureText: isVisible,
+                      controller: linkController,
+                      textDirection: TextDirection.ltr,
+                      onChanged: (value) {
+                        // print(value);
+                      },
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(
+                          Icons.lock,
+                        ),
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            isVisible = !isVisible;
+                            setState(() {});
+                          },
+                          child: Icon(
+                            isVisible ? Icons.visibility : Icons.visibility_off,
+                          ),
+                        ),
+                        hintText: 'Autologin link',
+                        labelText: 'Autologin link',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                            color: Colors.red,
+                            width: 1,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    errorMessage,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (linkController.text.isEmpty) {
+                        setState(() {
+                          errorMessage = 'Please fill the field';
+                        });
+                      } else {
+                        if (linkController.text.isEmpty) {
+                          if (linkController.text.isEmpty) {
+                            setState(() {
+                              errorMessage = 'Autologin is required';
+                            });
+                          }
+                        } else {
+                          errorMessage = '';
+                          global.isLoggedIn = true;
+                          global.autologinLink = linkController.text;
+                          getData(context);
+                        }
+                      }
+                    },
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                      child: Text('Submit'),
+                    ),
+                  ),
+                ],
               ),
             ),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 16,
-                ),
-                TextField(
-                  obscureText: isVisible,
-                  controller: linkController,
-                  onChanged: (value) {
-                    // print(value);
-                  },
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(
-                      Icons.lock,
-                    ),
-                    suffixIcon: GestureDetector(
-                      onTap: () {
-                        isVisible = !isVisible;
-                        setState(() {});
-                      },
-                      child: Icon(
-                        isVisible ? Icons.visibility : Icons.visibility_off,
-                      ),
-                    ),
-                    hintText: 'Autologin link',
-                    labelText: 'Autologin link',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: const BorderSide(
-                        color: Colors.red,
-                        width: 1,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Text(
-                  errorMessage,
-                  style: const TextStyle(color: Colors.red),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (linkController.text.isEmpty) {
-                      setState(() {
-                        errorMessage = 'Please fill the field';
-                      });
-                    } else {
-                      if (linkController.text.isEmpty) {
-                        if (linkController.text.isEmpty) {
-                          setState(() {
-                            errorMessage = 'Autologin is required';
-                          });
-                        }
-                      } else {
-                        errorMessage = '';
-                        global.isLoggedIn = true;
-                        global.autologinLink = linkController.text;
-                        getData(context);
-                      }
-                    }
-                  },
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                    child: Text('Submit'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,75 +1,124 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
 import 'package:applitech/app.dart';
 import 'package:applitech/pages/home.dart';
 import 'package:applitech/pages/login.dart';
-import 'package:applitech/pages/routes/calendar_page.dart';
-import 'package:applitech/pages/routes/help_page.dart';
-import 'package:applitech/pages/routes/notes_page.dart';
-import 'package:applitech/pages/routes/profile_page.dart';
 import 'package:applitech/pages/routes/settings_page.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:applitech/global/variables.dart' as global;
 
 void main() {
-  testWidgets(
-    'AppliTech Widget smoke test',
-    (WidgetTester tester) async {
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(const AppliTech());
-    },
-  );
-  testWidgets(
-    'Login Widget smoke test',
-    (WidgetTester tester) async {
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(const Login());
-    },
-  );
-  testWidgets(
-    'Home Widget smoke test',
-    (WidgetTester tester) async {
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(const Home());
-    },
-  );
-  testWidgets(
-    'Notes page smoke test',
-    (WidgetTester tester) async {
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(const NotesPage());
-    },
-  );
-  testWidgets(
-    'Profile page smoke test',
-    (WidgetTester tester) async {
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(const ProfilePage());
-    },
-  );
-  testWidgets(
-    'Calendar page smoke test',
-    (WidgetTester tester) async {
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(const CalendarPage());
-    },
-  );
-  testWidgets(
-    'Help page smoke test',
-    (WidgetTester tester) async {
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(const HelpPage());
-    },
-  );
-  testWidgets(
-    'AppliTech Widget smoke test',
-    (WidgetTester tester) async {
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(const AppliTech());
-    },
-  );
-  testWidgets(
-    'Settings page smoke test',
-    (WidgetTester tester) async {
-      // Build our app and trigger a frame.
-      await tester.pumpWidget(const SettingsPage());
-    },
-  );
+  // Check main
+  group("Main test : ", () {
+    // Check app
+
+    group("App test : ", () {
+      test('app.dart file', () {
+        expect(const AppliTech(), isNotNull);
+      });
+    });
+
+    // Check home page
+
+    group("Home test : ", () {
+      test('home.dart file', () {
+        expect(const Home(), isNotNull);
+      });
+    });
+
+    // Check Login page
+    group("Login test : ", () {
+      test('login.dart file', () {
+        expect(Login, isNotNull);
+      });
+      Widget makeTestableWidget(Widget widget) {
+        return const MediaQuery(
+          data: MediaQueryData(),
+          child: MaterialApp(
+            home: Login(),
+          ),
+        );
+      }
+
+      testWidgets('elevated buttons check', (WidgetTester tester) async {
+        await tester.pumpWidget(makeTestableWidget(const Login()));
+        expect(find.byType(ElevatedButton), findsWidgets);
+      });
+
+      testWidgets('texts widget check', (WidgetTester tester) async {
+        await tester.pumpWidget(makeTestableWidget(const Login()));
+        expect(find.byType(Text), findsWidgets);
+      });
+
+      testWidgets('column widget check', (WidgetTester tester) async {
+        await tester.pumpWidget(makeTestableWidget(const Login()));
+        expect(find.byType(Column), findsWidgets);
+      });
+
+      testWidgets('navigator widget check', (WidgetTester tester) async {
+        await tester.pumpWidget(makeTestableWidget(const Login()));
+        expect(find.byType(Navigator), findsWidgets);
+      });
+
+      testWidgets('center widget check', (WidgetTester tester) async {
+        await tester.pumpWidget(makeTestableWidget(const Login()));
+        expect(find.byType(Center), findsWidgets);
+      });
+
+      testWidgets('flexible widget check', (WidgetTester tester) async {
+        await tester.pumpWidget(makeTestableWidget(const Login()));
+        expect(find.byType(Flexible), findsWidgets);
+      });
+
+      testWidgets('sized box widget check', (WidgetTester tester) async {
+        await tester.pumpWidget(makeTestableWidget(const Login()));
+        expect(find.byType(SizedBox), findsWidgets);
+      });
+
+      testWidgets('material widget check', (WidgetTester tester) async {
+        await tester.pumpWidget(makeTestableWidget(const Login()));
+        expect(find.byType(Material), findsWidgets);
+      });
+
+      testWidgets('icons widget check', (WidgetTester tester) async {
+        await tester.pumpWidget(makeTestableWidget(const Login()));
+        expect(find.byType(Icon), findsWidgets);
+      });
+
+      testWidgets('padding check', (WidgetTester tester) async {
+        await tester.pumpWidget(makeTestableWidget(const Login()));
+        expect(find.byType(Padding), findsWidgets);
+      });
+    });
+
+    // Check Settings page
+
+    group("Settings page : ", () {
+      test('settings.dart file', () {
+        expect(const SettingsPage(), isNotNull);
+      });
+    });
+
+    // Check global
+
+    group("Global : ", () {
+      test('global.dart file', () {
+        expect(global.version, isNotEmpty);
+        expect(global.userData, isNull);
+        expect(global.homeData, isNull);
+        expect(global.calendarData, isNull);
+        expect(global.absencesData, isNull);
+        expect(global.notificationsData, isNull);
+        expect(global.isLoggedIn, isFalse);
+        expect(global.isThemeDark, isFalse);
+        expect(global.timerActivityComing, isNonZero);
+      });
+    });
+  });
 }
