6c7025b1f663a979da476e3bb062ae28e7e8e55b
Quentin TREHEUX <quentin.treheux@epitech.eu>
2022-03-03 02:50:22 +0100

    [UPD] fix disconnected && coding style

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -9,32 +9,26 @@ class CustomDrawer extends StatefulWidget {
 }
 
 class _CustomDrawerState extends State<CustomDrawer> {
-
-  Widget menuList(String title, String route, Icon icon)
-  {
-    return (
-      ListTile(
-        leading: icon,
-        title: Text(title),
-        onTap: () {
-          Navigator.of(context).popAndPushNamed(route);
-        },
-      )
-    );
+  Widget menuList(String title, String route, Icon icon) {
+    if (route == "/applitech") global.isLoggedIn = false;
+    return (ListTile(
+      leading: icon,
+      title: Text(title),
+      onTap: () {
+        Navigator.of(context).popAndPushNamed(route);
+      },
+    ));
   }
 
-  Widget displayInfoUser(String info, double size)
-  {
-    return (
-      Text(
-        info,
-        style: TextStyle(
-          fontSize: size,
-          fontWeight: FontWeight.bold,
-          color: global.isThemeDark ? Colors.black : Colors.white,
-        ),
-      )
-    );
+  Widget displayInfoUser(String info, double size) {
+    return (Text(
+      info,
+      style: TextStyle(
+        fontSize: size,
+        fontWeight: FontWeight.bold,
+        color: global.isThemeDark ? Colors.black : Colors.white,
+      ),
+    ));
   }
 
   @override
@@ -78,7 +72,8 @@ class _CustomDrawerState extends State<CustomDrawer> {
                   const SizedBox(height: 20),
                   displayInfoUser(global.userData['login'], 16),
                   const SizedBox(height: 20),
-                  displayInfoUser(global.userData['credits'].toString() + " crédits", 16),
+                  displayInfoUser(
+                      global.userData['credits'].toString() + " crédits", 16),
                 ],
               ),
             ),
@@ -86,10 +81,12 @@ class _CustomDrawerState extends State<CustomDrawer> {
           menuList('Acceuil', '/home', const Icon(Icons.menu_book)),
           menuList('Profil', '/profile', const Icon(Icons.person)),
           menuList('Notes', '/notes', const Icon(Icons.notes)),
-          menuList('Abscence', '/absences', const Icon(Icons.do_not_disturb_alt_outlined)),
+          menuList('Abscence', '/absences',
+              const Icon(Icons.do_not_disturb_alt_outlined)),
           menuList('Paramètres', '/settings', const Icon(Icons.settings)),
           menuList('Aide', '/help', const Icon(Icons.help)),
-          menuList('Se déconnecter', '/applitech', const Icon(Icons.exit_to_app)),
+          menuList(
+              'Se déconnecter', '/applitech', const Icon(Icons.exit_to_app)),
         ],
       ),
     );
